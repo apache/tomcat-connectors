e40709fd7304a9256a519bc1eab85295a2bb5df9
(no author) <(no author)@unknown>
2003-05-27 13:04:46 +0000

    This commit was manufactured by cvs2svn to create branch 'JK_1_2_4'.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/branches/JK_1_2_4@298703 13f79535-47bb-0310-9956-ffa450edef68

--- http11/src/test/test.bat
+++
@@ -1,4 +0,0 @@
-
-
-java -cp .;../../../lib/commons-logging.jar;../lib/tomcat-util.jar;../lib/tomcat-coyote.jar;../lib/tomcat-http11.jar org.apache.coyote.http11.FileTester tests/test1.txt tests/test1.out
-
