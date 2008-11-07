a9c836a70df89bdfcfce10761f35087c11bbee0e
Rainer Jung <rjung@apache.org>
2008-11-07 18:05:13 +0000

    Increment version to 1.2.28-dev after 1.2.27 release.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/connectors/trunk/jk@712212 13f79535-47bb-0310-9956-ffa450edef68

--- native/common/jk_version.h
+++ native/common/jk_version.h
@@ -26,11 +26,11 @@
 /************** START OF AREA TO MODIFY BEFORE RELEASING *************/
 #define JK_VERMAJOR     1
 #define JK_VERMINOR     2
-#define JK_VERFIX       27
-#define JK_VERSTRING    "1.2.27"
+#define JK_VERFIX       28
+#define JK_VERSTRING    "1.2.28"
 
 /* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 1
+#define JK_VERISRELEASE 0
 /* Beta number */
 #define JK_VERBETA      0
 #define JK_BETASTRING   "0"
