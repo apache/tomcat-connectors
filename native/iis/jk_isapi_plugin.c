99525f2e6faade0ca02734351a0aca44e366081e
Rainer Jung <rjung@apache.org>
2010-01-02 12:15:57 +0000

    Happy new year! Update Copyright year for JK.
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@895180 13f79535-47bb-0310-9956-ffa450edef68

--- native/iis/jk_isapi_plugin.c
+++ native/iis/jk_isapi_plugin.c
@@ -209,7 +209,7 @@ char HTML_ERROR_TAIL[] =        "<P>\n<BR/>&nbsp;<BR/>&nbsp;<BR/>&nbsp;<BR/>&nbs
                                 "<BR/>&nbsp;\n"
                                 "<HR/>\n"
                                 "<P id=\"footer\">\n"
-                                "Copyright &copy; 1999-2009 Apache Software Foundation<BR/>\n"
+                                "Copyright &copy; 1999-2010 Apache Software Foundation<BR/>\n"
                                 "All Rights Reserved\n"
                                 "</P>\n</BODY>\n</HTML>\n";
 
