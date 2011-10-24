apache/tomcat-connectors

commit 057e4bf156bc1460efe6efdae196627f4c790c50

BZ 51769: IIS: Allow URIs which contain "META-INF" or "WEB-INF" as long as they are not path components of the URI.
I kept the old stristr function, because it
could be useful for something else.


git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@1187916 13f79535-47bb-0310-9956-ffa450edef68

native/iis/jk_isapi_plugin.c
@@ -842,12 +842,30 @@ static char *stristr(const char *s, const char *find)
     return ((char *)s);
 }
 
+/*
+ * Find the first occurrence of path in uri tokenized by "/".
+ * The comparison is done case insensitive.
+ */
+static const char *find_path_in_uri(const char *uri, const char *path)
+{
+    size_t len = strlen(path);
+    while (uri = strchr(uri, '/')) {
+        uri++;
+        if (!strncmp(uri, path, len) &&
+            (*(uri + len) == '/' ||
+             strlen(uri) == len)) {
+            return uri;
+        }
+    }
+    return NULL;
+}
+
 static int uri_is_web_inf(const char *uri)
 {
-    if (stristr(uri, "/web-inf")) {
+    if (find_path_in_uri(uri, "web-inf")) {
         return JK_TRUE;
     }
-    if (stristr(uri, "/meta-inf")) {
+    if (find_path_in_uri(uri, "meta-inf")) {
         return JK_TRUE;
     }
 

commit 9192ac15301f058db8f60d3d4c309604a33e00ba

Fix bug introduced by r1187916 (case insensitive comparison switched to case sensitive).
git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@1188226 13f79535-47bb-0310-9956-ffa450edef68

native/iis/jk_isapi_plugin.c
@@ -851,7 +851,7 @@ static const char *find_path_in_uri(const char *uri, const char *path)
     size_t len = strlen(path);
     while (uri = strchr(uri, '/')) {
         uri++;
-        if (!strncmp(uri, path, len) &&
+        if (!strnicmp(uri, path, len) &&
             (*(uri + len) == '/' ||
              strlen(uri) == len)) {
             return uri;