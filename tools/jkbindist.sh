2e785669b9f2936c323d26e2aa8cdb837028b104
Mladen Turk <mturk@apache.org>
2012-03-09 06:32:02 +0000

    No need for ASL header in simple autogenerated README
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@1298714 13f79535-47bb-0310-9956-ffa450edef68

--- tools/jkbindist.sh
+++ tools/jkbindist.sh
@@ -102,23 +102,6 @@ rm -f ${copy} 2>/dev/null
 rm -f ${dist} 2>/dev/null
 rm -f ${dist}.* 2>/dev/null
 cat << EOF > README
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-
-
 Apache Tomcat Connectors - $version
 
 Here your'll find module binaries for $webdesc.
