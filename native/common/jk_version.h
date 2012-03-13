ecf9d7dceb0834cab2ff8e0d988b9a7879fd91b0
Mladen Turk <mturk@apache.org>
2012-03-13 17:21:16 +0000

    Bump the version to 1.2.34-dev
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@1300247 13f79535-47bb-0310-9956-ffa450edef68

--- native/common/jk_version.h
+++ native/common/jk_version.h
@@ -26,10 +26,10 @@
 /************** START OF AREA TO MODIFY BEFORE RELEASING *************/
 #define JK_VERMAJOR     1
 #define JK_VERMINOR     2
-#define JK_VERFIX       33
+#define JK_VERFIX       34
 
 /* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 1
+#define JK_VERISRELEASE 0
 /* Beta number */
 #define JK_VERBETA      0
 #define JK_BETASTRING   "0"
