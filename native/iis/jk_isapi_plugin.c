09e9a086056ac9478270292619388b3e586a75de
Rainer Jung <rjung@apache.org>
2018-01-04 17:25:22 +0000

    Happy New Year 2018
    git-svn-id: https://svn.apache.org/repos/asf/tomcat/jk/trunk@1820140 13f79535-47bb-0310-9956-ffa450edef68

--- native/iis/jk_isapi_plugin.c
+++ native/iis/jk_isapi_plugin.c
@@ -216,7 +216,7 @@ char HTML_ERROR_TAIL[] =        "<P>\n<BR/>&nbsp;<BR/>&nbsp;<BR/>&nbsp;<BR/>&nbs
                                 "<BR/>&nbsp;\n"
                                 "<HR/>\n"
                                 "<P id=\"footer\">\n"
-                                "Copyright &copy; 1999-2017 Apache Software Foundation<BR/>\n"
+                                "Copyright &copy; 1999-2018 Apache Software Foundation<BR/>\n"
                                 "All Rights Reserved\n"
                                 "</P>\n</BODY>\n</HTML>\n";
 
