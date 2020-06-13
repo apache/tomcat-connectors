0ff7479904e1a5447736b020a267b897ff826e1f
Mladen Turk <mturk@apache.org>
2020-06-13 19:07:26 +0200

    Drop all that beta,rc,release bloatware from jk_version.h. We use git, and release should be as simple as clicking on the webpage

--- native/common/jk_version.h
+++ native/common/jk_version.h
@@ -22,57 +22,34 @@
 #ifndef __JK_VERSION_H
 #define __JK_VERSION_H
 
-/************** START OF AREA TO MODIFY BEFORE RELEASING *************/
 #define JK_VERMAJOR     1
 #define JK_VERMINOR     2
-#define JK_VERFIX       49
+#define JK_VERMICRO     49
 
-/* set JK_VERISRELEASE to 1 when release (do not forget to commit!) */
-#define JK_VERISRELEASE 0
-/* Beta number */
-#define JK_VERBETA      0
-#define JK_BETASTRING   "0"
-/* Release candidate */
-#define JK_VERRC        0
-#define JK_RCSTRING     "0"
-
-/************** END OF AREA TO MODIFY BEFORE RELEASING *************/
-
-#if !defined(PACKAGE)
 #if defined(JK_ISAPI)
-#define PACKAGE "isapi_redirector"
+#define JK_DISTNAME "isapi_redirector"
 #define JK_DLL_SUFFIX "dll"
 #elif defined(JK_NSAPI)
-#define PACKAGE "nsapi_redirector"
+#define JK_DISTNAME "nsapi_redirector"
 #define JK_DLL_SUFFIX "dll"
 #else
-#define PACKAGE "mod_jk"
+#define JK_DISTNAME "mod_jk"
 #define JK_DLL_SUFFIX "so"
-#endif
 #endif
 
 /* Build JK_EXPOSED_VERSION and JK_VERSION */
-#define JK_EXPOSED_VERSION_INT PACKAGE "/" JK_VERSTRING
+#define JK_EXPOSED_VERSION JK_DISTNAME "/" JK_VERSTRING
 
-#if (JK_VERBETA != 0)
-#define JK_EXPOSED_VERSION JK_EXPOSED_VERSION_INT "-beta-" JK_BETASTRING
-#else
-#undef JK_VERBETA
-#define JK_VERBETA 255
-#if (JK_VERRC != 0)
-#define JK_EXPOSED_VERSION JK_EXPOSED_VERSION_INT "-rc-" JK_RCSTRING
-#elif (JK_VERISRELEASE == 1)
-#define JK_EXPOSED_VERSION JK_EXPOSED_VERSION_INT
-#else
-#define JK_EXPOSED_VERSION JK_EXPOSED_VERSION_INT "-dev"
+#if !defined(JK_VERSUFIX)
+#define JK_VERSUFIX ""
 #endif
-#endif
-#define JK_FULL_EXPOSED_VERSION JK_EXPOSED_VERSION
 
-#define JK_MAKEVERSION(major, minor, fix, beta) \
-            (((major) << 24) + ((minor) << 16) + ((fix) << 8) + (beta))
+#define JK_FULL_EXPOSED_VERSION JK_EXPOSED_VERSION JK_VERSUFIX
 
-#define JK_VERSION JK_MAKEVERSION(JK_VERMAJOR, JK_VERMINOR, JK_VERFIX, JK_VERBETA)
+#define JK_MAKEVERSION(major, minor, fix) \
+            (((major) << 24) + ((minor) << 16) + ((fix) << 8))
+
+#define JK_VERSION JK_MAKEVERSION(JK_VERMAJOR, JK_VERMINOR, JK_VERMICRO)
 
 /** Properly quote a value as a string in the C preprocessor */
 #define JK_STRINGIFY(n) JK_STRINGIFY_HELPER(n)
@@ -81,13 +58,12 @@
 #define JK_VERSTRING \
             JK_STRINGIFY(JK_VERMAJOR) "." \
             JK_STRINGIFY(JK_VERMINOR) "." \
-            JK_STRINGIFY(JK_VERFIX)
+            JK_STRINGIFY(JK_VERMICRO)
 
 /* macro for Win32 .rc files using numeric csv representation */
 #define JK_VERSIONCSV JK_VERMAJOR ##, \
                     ##JK_VERMINOR ##, \
-                    ##JK_VERFIX
+                    ##JK_VERMICRO
 
 
 #endif /* __JK_VERSION_H */
-
